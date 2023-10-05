2f43d3359fb7091b9568f2e652086d949718456c
Carlos Barros <carlos_dbs@hotmail.com>
2023-10-05 10:37:17 -0300

    feat(Signup):creates signup page

---
+++ Pages/Signup.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SunnysideAgency.Pages
+{
+    public class Signup : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
